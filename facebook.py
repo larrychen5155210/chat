cea34cabdcd3dfd79f705baf4655d05b421053ec
Larry Chen <z5155210@gmail.com>
2022-10-05 01:38:33 +0800

    add facebook.py

---
+++ facebook.py
@@ -0,0 +1,34 @@
+
+def read_file(filename):
+	lines=[]
+	with open(filename, 'r', encoding = 'utf-8-sig') as f:
+		for line in f:
+			lines.append(line.strip())
+	return lines
+
+def convert(lines): # 格式轉換
+	new = []
+	person = None
+	for line in lines:
+		if line == 'Allen':
+			person = 'Allen'
+			continue
+		elif line == 'Tom':
+			person = 'Tom'
+			continue
+		if person: # 如果 person 有值的話
+			new.append(person + ': ' + line)
+	return new
+
+def write_file(filename, lines):
+	with open(filename, 'w', encoding = 'utf-8-sig') as f:
+		for line in lines:
+			f.write(line + '\n')
+	
+def main():
+	lines = read_file('input.txt')
+	lines = convert(lines)
+	print(lines)
+	write_file('output.txt', lines)
+
+main()
